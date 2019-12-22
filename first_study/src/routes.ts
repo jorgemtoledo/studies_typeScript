868d30bf8aa1a6884d70f28eb8ee92fceff06715
jorgemtoledo <jorge.toledo@gmail.com>
2019-12-22 11:21:54 -0300

    Up files

---
+++ first_study/src/routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+
+import UserController from './Controllers/UserController'
+
+const routes = Router()
+
+routes.get('/users', UserController.index)
+routes.post('/users', UserController.create)
+
+export default routes
+
